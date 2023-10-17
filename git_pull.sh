6cab68660ef944265e544e937a40b04e4a679ae4
mountee32 <124308058+mountee32@users.noreply.github.com>
2023-10-17 14:07:09 +0100

    Update git_pull.sh

--- git_pull.sh
+++ git_pull.sh
@@ -2,6 +2,7 @@
 cd /home/andy/realise/realise
 GIT_BEFORE_PULL=$(git rev-parse HEAD)
 git reset --hard origin/main
+chmod +x /home/andy/realise/realise/git_pull.sh && chmod +x /home/andy/realise/realise/start.sh
 GIT_AFTER_PULL=$(git rev-parse HEAD)
 
 if [ "$GIT_BEFORE_PULL" != "$GIT_AFTER_PULL" ]; then
