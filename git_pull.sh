5f898e66984425183480f7efd99ec9faa96c79f0
mountee32 <124308058+mountee32@users.noreply.github.com>
2023-10-17 13:45:40 +0100

    Create git_pull.sh

---
+++ git_pull.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd /home/andy/realise
+GIT_BEFORE_PULL=$(git rev-parse HEAD)
+git pull
+GIT_AFTER_PULL=$(git rev-parse HEAD)
+
+if [ "$GIT_BEFORE_PULL" != "$GIT_AFTER_PULL" ]; then
+    /home/andy/realise/realise/start.sh
+fi
